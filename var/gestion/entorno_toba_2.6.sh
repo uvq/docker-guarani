b53aaba4b41bd5257574d3f047754a59320ce3a5
pablito <moltedopablo@gmail.com>
2016-06-02 12:10:41 -0300

    Agrego el export del locale para que el svn no tire errores dentro del container

--- var/gestion/entorno_toba_2.6.sh
+++ var/gestion/entorno_toba_2.6.sh
@@ -3,5 +3,6 @@ export TOBA_DIR=/var/local/gestion/lib/toba
 export TOBA_INSTANCIA=desarrollo
 export TOBA_INSTALACION_DIR=/var/local/gestion/docker-data/instalacion
 export PATH="$TOBA_DIR/bin:$PATH"
+export LC_CTYPE=C.UTF-8
 echo "Entorno cargado."
 echo "Ejecute 'toba' para ver la lista de comandos disponibles."
