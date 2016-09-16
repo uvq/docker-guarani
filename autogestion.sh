8952045345505bece5b63382f39b3827eb7fba81
pablito <moltedopablo@gmail.com>
2016-09-16 11:55:04 -0300

    Cambio para que no se sobreescriba config y login.php

--- autogestion.sh
+++ autogestion.sh
@@ -2,9 +2,10 @@
 
 HOME_3W=/var/local/autogestion
 
-cp /var/local/autogestion_conf/* ${HOME_3W}/instalacion
-cp ${HOME_3W}/instalacion/login_template.php ${HOME_3W}/instalacion/login.php
-chown -R www-data ${HOME_3W}/instalacion/log;
+if [ ! -f "$HOME_3W/instalacion/config.php" ]; then
+    cp /var/local/autogestion_conf/* $HOME_3W/instalacion
+    cp $HOME_3W/instalacion/login_template.php $HOME_3W/instalacion/login.php
+fi
+chown www-data $HOME_3W/instalacion/log -R
 
-
-ln -s ${HOME_3W}/instalacion/alias.conf /etc/apache2/sites-enabled/autogestion.conf
+ln -s $HOME_3W/instalacion/alias.conf /etc/apache2/sites-enabled/autogestion.conf
