cf7a7876011322c0beec49e4ff282f878cd8849f
pablito <moltedopablo@gmail.com>
2018-02-27 16:56:07 -0300

    Agregamos instalacion de composer

--- autogestion.sh
+++ autogestion.sh
@@ -5,6 +5,8 @@ HOME_3W=/var/local/autogestion
 if [ ! -f "$HOME_3W/instalacion/config.php" ]; then
     cp /var/local/autogestion_conf/* $HOME_3W/instalacion
     cp $HOME_3W/instalacion/login_template.php $HOME_3W/instalacion/login.php
+    cd $HOME_3W
+    composer install
 fi
 chown www-data $HOME_3W/instalacion/log -R
 
